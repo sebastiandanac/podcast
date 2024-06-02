1d9e14bfc0873688b8ec837b2ac669ec785e37e1
sebastiandanac <sebastiandanac@gmail.com>
2024-06-03 01:54:36 +0200

    feat: disable schedulling during weekend on server

--- server/insert-podcast.ts
+++ server/insert-podcast.ts
@@ -11,9 +11,19 @@ function isWithinSchedulableHours(timestamp: string) {
   return hour >= 8 && hour < 17;
 }
 
+function isWeekend(timestamp: string) {
+  const date = new Date(timestamp);
+  const day = date.getDay(); // 0 = Sunday, 6 = Saturday
+  return day === 0 || day === 6;
+}
+
 export async function insertPodcast(data: PodcastType['Insert']) {
   if (!isWithinSchedulableHours(data.date_time)) {
     throw new Error('Podcast must be scheduled within 8am-5pm.');
+  }
+
+  if (isWeekend(data.date_time)) {
+    throw new Error('Podcast can be scheduled only on weekdays.');
   }
 
   try {
