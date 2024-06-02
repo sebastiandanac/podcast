72453823e8a0a29293b692a24f90d5dac9990a2a
sebastiandanac <sebastiandanac@gmail.com>
2024-06-03 01:53:48 +0200

    feat: add date time to table

--- app/(app)/dashboard/columns.tsx
+++ app/(app)/dashboard/columns.tsx
@@ -5,6 +5,7 @@ import { PodcastsType } from '@/types/podcasts';
 import Status from '@/app/(app)/dashboard/status';
 import { Button } from '@/components/ui/button';
 import { ArrowUpDown } from 'lucide-react';
+import { format } from 'date-fns';
 
 export const columns: ColumnDef<PodcastsType>[] = [
   {
@@ -28,6 +29,11 @@ export const columns: ColumnDef<PodcastsType>[] = [
   {
     accessorKey: 'host_name',
     header: 'Host name',
+  },
+  {
+    accessorKey: 'date_time',
+    header: 'Date & time',
+    cell: ({ row }) => format(row.original.date_time, 'Pp'),
   },
   {
     accessorKey: 'status_id.name',
